087bc4abe8b65cce54dac0eead7945a70809f036
Sandy Wyper <sandywyper@gmail.com>
2018-12-29 12:11:59 +0000

    responsive css added

--- main.js
+++ main.js
@@ -142,3 +142,13 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+/* Toggle between adding and removing the "responsive" class to nav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("menu");
+  if (x.className === "menu") {
+    x.className += " responsive";
+  } else {
+    x.className = "menu";
+  }
+}
